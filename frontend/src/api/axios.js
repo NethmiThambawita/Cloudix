024ca92e406000d52a40aeb57ebf161e9742d5f8
Nethmi Thambawita <it22911544@my.sliit.lk>
2025-12-23 21:00:50 +0530

    changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cloudix-new.onrender.com/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1',
   headers: {
     'Content-Type': 'application/json'
   },
-  withCredentials: true
+  withCredentials: true,
+  timeout: 30000 // 30 second timeout
 });
 
 api.interceptors.request.use(
--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -2,29 +2,41 @@ import React, { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { Spin } from 'antd';
-import { fetchMe } from '../redux/slices/authSlice';
+import { Spin, Alert } from 'antd';
+import { fetchMe, logout } from '../redux/slices/authSlice';
 
 function PrivateRoute({ children }) {
   const dispatch = useDispatch();
-  const { token, user, loading } = useSelector((state) => state.auth);
+  const { token, user, loading, error } = useSelector((state) => state.auth);
 
   useEffect(() => {
-    if (token && !user) {
+    if (token && !user && !loading && !error) {
       dispatch(fetchMe());
     }
-  }, [token, user, dispatch]);
+  }, [token, user, loading, error, dispatch]);
 
-  if (!token) {
+  // If there's an error fetching user, clear token and redirect to login
+  useEffect(() => {
+    if (error && token) {
+      dispatch(logout());
+    }
+  }, [error, token, dispatch]);
+
+  if (!token || error) {
     return <Navigate to="/login" replace />;
   }
 
   if (loading && !user) {
     return (
       <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <Spin size="large" />
+        <Spin size="large" tip="Loading..." />
       </div>
     );
+  }
+
+  // If we have a token but still no user after loading is done, something went wrong
+  if (!loading && !user) {
+    return <Navigate to="/login" replace />;
   }
 
   return children;
